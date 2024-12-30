932dd216bccd3c9adff96659c30f7b1e2c71f5d0
Ayrton Surica <ayrtonsurica@gmail.com>
2024-12-30 00:05:14 -0300

    feat: criando controlador para services

---
+++ controllers/service_controller.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"example/APIForWorldWorkHub/models"
+	"github.com/go-playground/validator/v10"
+)
+
+func AddService(context *gin.Context, validate *validator.Validate) {
+	var newService models.Service
+	if err := context.BindJSON(&newService); err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid JSON"})
+		return
+	}
+
+	// Validate the service
+	if err := validate.Struct(newService); err != nil {
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Validation failed", "errors": err.Error()})
+		return
+	}
+
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		context.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Failed to connect database"})
+		return
+	}
+	defer db.Close()
+
+	// Verificar se o usuário existe
+	var user models.User
+	if err := db.Where("id = ?", newService.UserID).First(&user).Error; err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+		return
+	}
+
+	if err := db.Create(&newService).Error; err != nil {
+		context.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "Failed to create service"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusCreated, newService)
+}
