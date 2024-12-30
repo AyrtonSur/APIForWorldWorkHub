fea5ef1f8a8b9ea404066cadf268118afd3fd1e8
Ayrton Surica <ayrtonsurica@gmail.com>
2024-12-30 00:07:02 -0300

    feat: criação de arquivo para rotas

---
+++ routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"example/APIForWorldWorkHub/controllers"
+)
+
+func SetupRoutes(router *gin.Engine, validate *validator.Validate) {
+	router.GET("/users", controllers.GetUsers)
+	router.POST("/users", func(c *gin.Context) {
+		controllers.AddUser(c, validate)
+	})
+	router.GET("/users/:id", controllers.GetUser)
+	router.POST("/services", func(c *gin.Context) {
+		controllers.AddService(c, validate)
+	})
+}
