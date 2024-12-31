c390350edd26d2728f958ef5fcad9e8e105e1264
Ayrton Surica <ayrtonsurica@gmail.com>
2024-12-31 19:38:34 -0300

    fix: atualizando migração

--- database/migration.go
+++ database/migration.go
@@ -2,18 +2,17 @@ package database
 
 import (
 	"fmt"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 	"example/APIForWorldWorkHub/models"
 )
 
 func InitialMigration() {
-	db, err := gorm.Open("sqlite3", "test.db")
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("failed to connect database")
 	}
-	defer db.Close()
 
 	db.AutoMigrate(&models.User{}, &models.Service{}, &models.Language{}, &models.Occupation{})
 }
