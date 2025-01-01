b848a8551d383c283892074eafd22a778744579d
Ayrton Surica <ayrtonsurica@gmail.com>
2024-12-31 21:37:10 -0300

    feat: utilizando middleware authenticate em rotas específicas

--- routes/routes.go
+++ routes/routes.go
@@ -4,15 +4,26 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"example/APIForWorldWorkHub/controllers"
+	"example/APIForWorldWorkHub/middlewares"
 )
 
 func SetupRoutes(router *gin.Engine, validate *validator.Validate) {
-	router.GET("/users", controllers.GetUsers)
+	router.POST("/login", func(c *gin.Context) {
+		controllers.Login(c, validate)
+	})
+
 	router.POST("/users", func(c *gin.Context) {
 		controllers.Register(c, validate)
 	})
-	router.GET("/users/:id", controllers.GetUser)
-	router.POST("/services", func(c *gin.Context) {
-		controllers.AddService(c, validate)
-	})
+
+	auth := router.Group("/")
+	auth.Use(middlewares.Authenticate())
+	{
+		auth.GET("/users", controllers.GetUsers)
+
+		auth.GET("/users/:id", controllers.GetUser)
+		auth.POST("/services", func(c *gin.Context) {
+			controllers.AddService(c, validate)
+		})
+	}
 }
