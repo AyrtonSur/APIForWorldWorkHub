33a78d4c4fa7df0cec5d9217acdc17e0282c0896
Ayrton Surica <ayrtonsurica@gmail.com>
2025-01-01 03:59:15 -0300

    feat: criando algumas ocupações

--- database/migration.go
+++ database/migration.go
@@ -18,4 +18,41 @@ func InitialMigration() {
 	}
 
 	DB.AutoMigrate(&models.User{}, &models.Service{}, &models.Language{}, &models.Occupation{})
+
+	occupations := []models.Occupation{
+		{Name: "Cleaner"},
+		{Name: "Lawyer"},
+		{Name: "Doctor"},
+		{Name: "Engineer"},
+		{Name: "Teacher"},
+		{Name: "Nurse"},
+		{Name: "Accountant"},
+		{Name: "Architect"},
+		{Name: "Chef"},
+		{Name: "Dentist"},
+		{Name: "Electrician"},
+		{Name: "Mechanic"},
+		{Name: "Pharmacist"},
+		{Name: "Plumber"},
+		{Name: "Software Developer"},
+		{Name: "Graphic Designer"},
+		{Name: "Journalist"},
+		{Name: "Photographer"},
+		{Name: "Pilot"},
+		{Name: "Police Officer"},
+		{Name: "Scientist"},
+		{Name: "Veterinarian"},
+		{Name: "Writer"},
+		{Name: "Musician"},
+		{Name: "Artist"},
+		{Name: "Therapist"},
+		{Name: "Translator"},
+		{Name: "Web Developer"},
+		{Name: "Data Analyst"},
+		{Name: "Marketing Specialist"},
+	}
+
+	for _, occupation := range occupations {
+		DB.FirstOrCreate(&occupation, models.Occupation{Name: occupation.Name})
+	}
 }
