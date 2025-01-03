3326b40b6da847373163b7a0f86b93ec82a61d9c
Ayrton Surica <ayrtonsurica@gmail.com>
2025-01-02 21:44:18 -0300

    fix: simplificando main para que fique menor

--- main.go
+++ main.go
@@ -1,46 +1,18 @@
 package main
 
 import (
-	"regexp"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
-	"example/APIForWorldWorkHub/utils"
 	"example/APIForWorldWorkHub/database"
 	"example/APIForWorldWorkHub/routes"
+	"example/APIForWorldWorkHub/utils"
 )
 
-var validate *validator.Validate
-
 func main() {
-	fmt.Println("Go ORM Tutorial")
-	validate = validator.New()
-	validate.RegisterValidation("zipcode", validateZipCode)
-	validate.RegisterValidation("password", passwordValidator) // Registrar a validação personalizada
+	utils.InitValidator()
 	database.InitialMigration()
 	router := gin.Default()
-	routes.SetupRoutes(router, validate)
+	routes.SetupRoutes(router)
 	router.Run("localhost:9090")
-}
-
-func validateZipCode(fl validator.FieldLevel) bool {
-	zipCode := fl.Field().String()
-
-	regex := regexp.MustCompile(`^\d{5}(?:-\d{4})?$`)
-	if !regex.MatchString(zipCode) {
-		return false
-	}
-
-	// Verifica se o ZipCode realmente existe
-	exists, err := utils.ValidateZipCode(zipCode)
-	if err != nil || !exists {
-		return false
-	}
-
-	return true
-}
-
-func passwordValidator(fl validator.FieldLevel) bool {
-	password := fl.Field().String()
-	return len(password) >= 4
+	fmt.Println("Server Running in localhost:9090")
 }
