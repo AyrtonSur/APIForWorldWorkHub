28124e5a1d3b3d32f295e38ca75510529ea6af43
Ayrton Surica <ayrtonsurica@gmail.com>
2025-01-02 21:43:54 -0300

    fix: atualizando com base nas alterações em validator, user_controller e service_controller

--- routes/routes.go
+++ routes/routes.go
@@ -2,28 +2,20 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator/v10"
 	"example/APIForWorldWorkHub/controllers"
 	"example/APIForWorldWorkHub/middlewares"
 )
 
-func SetupRoutes(router *gin.Engine, validate *validator.Validate) {
-	router.POST("/login", func(c *gin.Context) {
-		controllers.Login(c, validate)
-	})
+func SetupRoutes(router *gin.Engine) {
+	router.POST("/login", controllers.Login)
 
-	router.POST("/users", func(c *gin.Context) {
-		controllers.Register(c, validate)
-	})
+	router.POST("/users", controllers.Register)
 
 	auth := router.Group("/")
 	auth.Use(middlewares.Authenticate())
 	{
 		auth.GET("/users", controllers.GetUsers)
-
 		auth.GET("/users/:id", controllers.GetUser)
-		auth.POST("/services", func(c *gin.Context) {
-			controllers.AddService(c, validate)
-		})
+		auth.POST("/services", controllers.AddService)
 	}
 }
