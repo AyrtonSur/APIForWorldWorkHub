3308e7994b6257a6c3d55fca7eb3bf874abd5cc9
Ayrton Surica <ayrtonsurica@gmail.com>
2025-01-04 00:47:52 -0300

    feat: fazendo CPF ser unico no banco de dados

--- models/user.go
+++ models/user.go
@@ -6,23 +6,23 @@ import (
 )
 
 type User struct {
-	ID               string     `gorm:"type:uuid;primary_key"`
-	Firstname        string     `json:"firstname" validate:"required"`
-	Lastname         string     `json:"lastname" validate:"required"`
-	Email            string     `json:"email" gorm:"uniqueIndex" validate:"required,email"`
-	PasswordDigest   string     `json:"password_digest" validate:"required,password"`
-	CPF              *string    `json:"CPF"`
-	Role             string     `json:"role" validate:"required"`
+	ID               string      `gorm:"type:uuid;primary_key"`
+	Firstname        string      `json:"firstname" validate:"required"`
+	Lastname         string      `json:"lastname" validate:"required"`
+	Email            string      `json:"email" gorm:"uniqueIndex" validate:"required,email"`
+	PasswordDigest   string      `json:"password_digest" validate:"required,password"`
+	CPF              *string     `json:"CPF" gorm:"uniqueIndex"`
+	Role             string      `json:"role" validate:"required"`
 	OccupationID     *string       
 	Occupation       *Occupation `gorm:"foreignKey:OccupationID"`
-	Phone            string     `json:"phone"`
-	ZipCode          string     `json:"zipcode"`
-	Education        string     `json:"education"`
+	Phone            string      `json:"phone"`
+	ZipCode          string      `json:"zipcode"`
+	Education        string      `json:"education"`
 	RegionID         string
-	Region           Region     `gorm:"foreignKey:RegionID"`
-	City             string     `json:"city"`
-	Services         []Service  `gorm:"foreignkey:UserID"`
-	SpokenLanguages  []Language `gorm:"foreignkey:UserID"`
+	Region           Region      `gorm:"foreignKey:RegionID"`
+	City             string      `json:"city"`
+	Services         []Service   `gorm:"foreignkey:UserID"`
+	SpokenLanguages  []Language  `gorm:"foreignkey:UserID"`
 }
 
 func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
