8b46a156c28ecb258008449c9ea23f26f60749aa
Ayrton Surica <ayrtonsurica@gmail.com>
2025-01-05 00:18:53 -0300

    feat: modificando AddService para poder receber um float

--- controllers/service_controller.go
+++ controllers/service_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"time"
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"example/APIForWorldWorkHub/database"
@@ -9,23 +10,36 @@ import (
 )
 
 func AddService(context *gin.Context) {
-	var newService models.Service
-	if err := context.BindJSON(&newService); err != nil {
+	var input struct {
+		UserID      string    `json:"userId" validate:"required"`
+		Date        time.Time `json:"date" validate:"required"`
+		Pay         float64   `json:"pay" validate:"required,min=0"`
+		Description string    `json:"description" validate:"required"`
+	}
+
+	if err := context.BindJSON(&input); err != nil {
 		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid JSON"})
 		return
 	}
 
 	// Validate the service
-	if err := utils.Validate.Struct(newService); err != nil {
+	if err := utils.Validate.Struct(input); err != nil {
 		context.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Validation failed", "errors": err.Error()})
 		return
 	}
 
 	// Verificar se o usuário existe
 	var user models.User
-	if err := database.DB.Where("id = ?", newService.UserID).First(&user).Error; err != nil {
+	if err := database.DB.Where("id = ?", input.UserID).First(&user).Error; err != nil {
 		context.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
 		return
+	}
+
+	newService := models.Service{
+		UserID:      input.UserID,
+		Date:        input.Date,
+		Pay:         int64(input.Pay * 100),
+		Description: input.Description,
 	}
 
 	if err := database.DB.Create(&newService).Error; err != nil {
