9b094ea1802173ba78ec816e1c324d4955bf70c3
Ayrton Surica <ayrtonsurica@gmail.com>
2025-01-06 20:47:48 -0300

    feat: criando uma tabela para permissões

---
+++ models/permission.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Permission struct {
+	ID   string   `gorm:"primaryKey"`
+	Name string `gorm:"unique;not null"`
+}
+
+func (permission *Permission) BeforeCreate(tx *gorm.DB) (err error) {
+	permission.ID = uuid.New().String()
+	return
+}
