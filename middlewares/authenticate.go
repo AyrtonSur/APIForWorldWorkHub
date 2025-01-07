a9f4ef9ea6d902d6fc5c54037c538d86eca681be
Ayrton Surica <ayrtonsurica@gmail.com>
2025-01-07 20:32:34 -0300

    fix: mudando mensagem de erro

--- middlewares/authenticate.go
+++ middlewares/authenticate.go
@@ -10,14 +10,14 @@ func Authenticate() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		tokenString := context.GetHeader("Authorization")
 		if tokenString == "" {
-			context.JSON(http.StatusUnauthorized, gin.H{"error": "Token necessário"})
+			context.JSON(http.StatusUnauthorized, gin.H{"error": "Token is necessary"})
 			context.Abort()
 			return
 		}
 
 		claims, err := utils.ValidateJWT(tokenString)
 		if err != nil || claims == nil {
-			context.JSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			context.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Token"})
 			context.Abort()
 			return
 		}
