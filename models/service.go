c6d64164afa90fc0ff2cde45857c65f1a5a6f04c
Ayrton Surica <ayrtonsurica@gmail.com>
2025-02-01 21:51:32 -0300

    feat: atualizando tabela service

--- models/service.go
+++ models/service.go
@@ -8,7 +8,7 @@ import (
 
 type Service struct {
 	ID          string    `gorm:"type:uuid;primary_key"`
-	UserID      string    `json:"userId" gorm:"type:uuid" validate:"required"`
+	UserID      string    `json:"userId" gorm:"type:uuid;constraint:OnDelete:CASCADE;" validate:"required"`
 	Date        time.Time `json:"date" validate:"required"`
 	Pay         int64     `json:"pay" validate:"required,min=0"`
 	Description string    `json:"description" validate:"required"`
